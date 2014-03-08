945568ea7697905616bf8d187395c40cf881a041
Kenichi Tachibana <deadzone@be.to>
2014-03-08 16:21:50 +0900

    ちゃんとしたMigrationが作られたので、やっぱり直接触るのはやめ

--- db/migrate/20140308065506_add_game_id.rb
+++ db/migrate/20140308065506_add_game_id.rb
@@ -1,5 +1,6 @@
 class AddGameId < ActiveRecord::Migration
   def change
-  	add_column :hexes,:game_id,:integer
+  	add_column :games,:geme_id,:integer
+  	add_column :hexes,:geme_id,:integer
   end
 end
