f0fe2505d17ce9b00f7760b0d16ecc7c26de3516
suchi <suchi@suchi.org>
2014-03-08 23:17:18 +0900

    i

--- app/controllers/hexes_controller.rb
+++ app/controllers/hexes_controller.rb
@@ -3,6 +3,8 @@ class HexesController < ApplicationController
     @game = Game.find(params[:game_id])
     @hex = @game.hexes.find(params[:id])
     @hex.assign_attributes(hex_params)
+    (x, y) = get_xy(params[:id].to_i)
+    
     if @hex.save
       redirect_to @game
     end
@@ -13,7 +15,7 @@ class HexesController < ApplicationController
     params.require(:hex).permit(:word)
   end
 
-  def getxy(i)
+  def get_xy(i)
     ax = (i - 1) % 11
     ay = (i - 1) / 11
     y = ay * 2
@@ -25,7 +27,7 @@ class HexesController < ApplicationController
     return x, y
   end
 
-  def getindex(x, y)
+  def get_index(x, y)
     if y % 2 == 1 then
       x += 5
     end
