13556873bd2b73f18e471de8a75ed2d63ea9b77c
diver42 <d4suzuki@gmail.com>
2014-03-08 13:01:52 +0900

    login Created

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Wordgame::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #config/environments/development.rb
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
