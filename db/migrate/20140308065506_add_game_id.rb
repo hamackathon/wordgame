5876cd58bc1a4b4f56f877a59d08cb970d6bec93
yowasou <jun.makita@gmail.com>
2014-03-09 10:32:23 +0900

    for heroku

--- db/migrate/20140308065506_add_game_id.rb
+++
@@ -1,6 +0,0 @@
-class AddGameId < ActiveRecord::Migration
-  def change
-  	add_column :games,:geme_id,:integer
-  	add_column :hexes,:geme_id,:integer
-  end
-end
--- db/migrate/20140308065943_remove_games_game_id.rb
+++
@@ -1,9 +0,0 @@
-class RemoveGamesGameId < ActiveRecord::Migration
-  def up
-    remove_column :games, :game_id
-      end
-
-  def down
-    add_column :games, :game_id, :integer
-  end
-end
