b6c03ba13f341cd3ea036ecd995a08654a338ff7
yowasou <jun.makita@gmail.com>
2014-03-12 20:04:21 +0900

    シークレットキーを環境変数にした

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wordgame::Application.config.secret_token = '530bcd7c9c63df9207b56947ab15c7b2c3362a64c8016abd81e2136901fa55b0edf9c309a2aaec31c42c23b6ea061b03a180cb84d314f373c383f48cb025ab90'
-Devise.secret_key = '530bcd7c9c63df9207b56947ab15c7b2c3362a64c8016abd81e2136901fa55b0edf9c309a2aaec31c42c23b6ea061b03a180cb84d314f373c383f48cb025ab90'
+Wordgame::Application.config.secret_token = ENV["SEACRET_KEY"]
+Devise.secret_key = ENV["SEACRET_KEY"]
